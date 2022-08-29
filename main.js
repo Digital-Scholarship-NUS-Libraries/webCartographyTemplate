b325160f8fafddb44723b6b03cbdd9a2174d81f5
gtnbssn <gaetan.boisson@gmail.com>
2022-08-29 17:33:52 +0800

    add markers

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
-import './style.css'
+import "./style.css";
 import "ol/ol.css";
 
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import { Map, View, Overlay } from "ol";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import { OSM as OSMSource, Vector as VectorSource } from "ol/source";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import { Icon, Style } from "ol/style";
 import { fromLonLat } from "ol/proj";
+import { csv as csvFetch } from "d3-fetch";
 
 const map = new Map({
-  target: 'map',
+  target: "map",
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
+      source: new OSMSource(),
+    }),
   ],
   view: new View({
     center: fromLonLat([103.8, 1.35]),
-    zoom: 12
-  })
+    zoom: 12,
+  }),
 });
+
+const addParksMarkers = async () => {
+  const parksList = await csvFetch("./data/parks.csv");
+
+  parksList.map((item)=>{
+    const overlayElement = document.createElement("img");
+    overlayElement.setAttribute("src", item.iconUrl);
+    overlayElement.setAttribute("class", "rounded-full transition-all border-4 border-emerald-500 hover:border-8 hover:border-emerald-100");
+    const olOverlay = new Overlay({
+      stopEvent: false,
+      positioning: "center-center",
+      element: overlayElement,
+    });
+    olOverlay.setPosition(fromLonLat([item.lon,item.lat]));
+    map.addOverlay(olOverlay);
+  })
+
+};
+
+addParksMarkers();
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
+  safelist: ["transition-all", "rounded-full", "border-4", "border-emerald-500", "hover:border-8", "hover:border-emerald-100"],
   theme: {
     extend: {},
   },
